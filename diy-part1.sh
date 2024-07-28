fe7345c853d3f0337bf564e78f6d94b4647d1dbc
suanfalalala <58913263+suanfalalala@users.noreply.github.com>
2024-07-28 21:49:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
 # echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >> feeds.conf.default
