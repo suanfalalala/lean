d74ba986667a8d6ae35d523ba73d4d594c1349a3
suanfalalala <58913263+suanfalalala@users.noreply.github.com>
2024-10-22 23:52:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-netgear/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-netgear/' feeds/luci/collections/luci-light/Makefile
